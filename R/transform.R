a8e1ac1f657371f3c98443b81f3b417d9510bcdb
Joe Fang (zhoufang) <zhoufang@micron.com>
2020-02-21 16:22:31 -0700

    add guard clause in move_left

--- R/transform.R
+++ R/transform.R
@@ -50,7 +50,7 @@ trim_spaces <- function(x,
 #' View(move_left(mtcars, c("gear", "carb", "wt")))
 
 move_left <- function(df, str) {
-  if (all(str %in% names(df)) == TRUE){
+  if ((all(str %in% names(df)) == TRUE) & (ncol(df) > 1)){
     col.indx.part1 <- sapply(seq_along(str), function(i) {
       which(str[i] == names(df))
     })
@@ -334,6 +334,39 @@ get_id <- function(df, ...) {
     names(df$tag)[df$tag == x])
 }
 
+# regular expressions -------------------------------------------------------------------------
+
+str_find <- function(str,
+                     start,
+                     end,
+                     replacement,
+                     return_loc = FALSE) {
+  #'wrapper function for locate a string based on regex leading to and after it
+  #'@param str a string to search from
+  #'@param start a regex string that lead to the string of interest
+  #'@param end a regex string that is after the strin gof interest
+  #'@param replacement a string to replace the located string, optional.
+  #'@return_loc a logical, if TRUE then return character start/end location instead of the string itself
+  #'
+  start_loc <- stringr::str_locate(str, start)[[2]] + 1
+  end_loc <- stringr::str_locate(str, end)[[1]] - 1
+
+  if (return_loc == TRUE) {
+    return(c("start" = start_loc,
+             "end" = end_loc))
+  }
+
+  if (hasArg(replacement)) {
+    part1 <- substr(str, 1, start_loc - 1)
+    part2 <- replacement
+    part3 <- substr(str, end_loc + 1, nchar(str))
+    return(paste0(part1, part2, part3))
+  }
+
+  if (!hasArg(replacement)) {
+    return(substr(str, start_loc, end_loc))
+  }
+}
 
 # tricks --------------------------------------------------------------------------------------
 
