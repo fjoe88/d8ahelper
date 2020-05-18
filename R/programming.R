e12230122c694c807d88bb153eea98d0fdf5af3d
FANG <ZHOUFANG@MICRON.COM>
2020-05-18 15:09:31 -0600

    +is_fct_or_chr

--- R/programming.R
+++ R/programming.R
@@ -11,14 +11,18 @@ lapply_preserve_names <- function(list, fun){
   })
 }
 
-list.files2 <- function(path = readr::clipboard(), return_str = TRUE, ...) {
-  #' wraper function list.files to allow coppied windows path format containing '/'
+list_files_fwd_slash <- function(path = readr::clipboard(), return_str = TRUE, ...) {
+  #' wraper function list.files to allow coppied windows path format containing backward slashes
 
   path <- stringr::str_replace_all(path, "\\\\+", "/")
 
-  if(return_str == TRUE){
+  if (return_str == TRUE){
     return(path)
   } else {
     list.files(path = path, ...)
   }
+}
+
+is_fct_or_chr <- function(x) {
+  is.factor(x) | is.character(x)
 }
